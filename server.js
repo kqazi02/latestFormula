c15213a82958ed64e2fe3d966ca8039c538f9db1
Kashan Ali Qazi <kashan.qazi02@utexas.edu>
2017-03-11 15:56:04 -0600

    initialized the app. Installed npm packages and setup the folders

---
+++ server.js
@@ -0,0 +1,25 @@
+//initialize the npm packages
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var request = require("request");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
+
+// Use morgan and body parser with our app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Make public a static dir
+app.use(express.static("public"));
+
+// deploy the app
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
